3625484f458fbc11fcdde7377a87eb694b7db546
Hamilton Lima <hamilton.lima@indexexchange.com>
2018-06-24 11:33:40 -0400

    Adds unit test and fixes to RemoveSession

--- sessionmanager/sessionmanager.go
+++ sessionmanager/sessionmanager.go
@@ -1,6 +1,7 @@
 package sessionmanager
 
 import (
+	"fmt"
 	"net/url"
 	"strings"
 	"sync"
@@ -22,8 +23,7 @@ func NewSessionManager() *SessionManager {
 	var m = make(map[string][]*melody.Session)
 	result.sessions = &m
 	result.lock = &sync.Mutex{}
-	var lastSessionID uint64
-	result.lastSessionID = &lastSessionID
+	result.lastSessionID = new(uint64)
 
 	return &result
 }
@@ -43,38 +43,50 @@ func (manager *SessionManager) GetSessions(matchID string) []*melody.Session {
 // AddSession add a new session
 func (manager *SessionManager) AddSession(session *melody.Session) {
 	var matchID = manager.GetIDFromURL(session.Request.URL)
-	session.Set("ID", manager.nextID())
+	session.Set("ID", manager.NextID())
 	manager.lock.Lock()
 	(*manager.sessions)[matchID] = append((*manager.sessions)[matchID], session)
 	manager.lock.Unlock()
 }
 
 // RemoveSession removes a session from the current list
+// TODO: use another structure to support several removal operations
+// and regenerate the session array from it
 func (manager *SessionManager) RemoveSession(session *melody.Session) {
-	// TODO: FIX THIS
-
 	var matchID = manager.GetIDFromURL(session.Request.URL)
-	var ID2Remove, _ = session.Get("ID")
+	var ID2Remove = manager.GetIDFromSession(session)
 
 	manager.lock.Lock()
 	var size = len((*manager.sessions)[matchID]) - 1
-	var updatedSessions = make([]*melody.Session, size, size)
-	var pos = 0
-	for _, slice := range (*manager.sessions)[matchID] {
-		var ID, _ = slice.Get("ID")
-		if *ID.(*uint64) != *ID2Remove.(*uint64) {
-			updatedSessions[pos] = slice
-			pos++
+	var updatedSessions []*melody.Session
+
+	if size > 0 {
+		updatedSessions = make([]*melody.Session, size, size)
+		for _, slice := range (*manager.sessions)[matchID] {
+			var ID = manager.GetIDFromSession(slice)
+			fmt.Printf("ID=%v ID2Remove=%v,", ID, ID2Remove)
+
+			if ID != ID2Remove {
+				updatedSessions = append(updatedSessions, slice)
+			}
 		}
+	} else {
+		updatedSessions = make([]*melody.Session, 0, 0)
 	}
 
 	(*manager.sessions)[matchID] = updatedSessions
-
 	manager.lock.Unlock()
 }
 
-// nextID calculates the next session ID
-func (manager *SessionManager) nextID() *uint64 {
+// GetIDFromSession returns the ID from the Session
+func (manager *SessionManager) GetIDFromSession(session *melody.Session) uint64 {
+	var result, _ = session.Get("ID")
+	var pointer = result.(*uint64)
+	return *pointer
+}
+
+// NextID calculates the next session ID
+func (manager *SessionManager) NextID() *uint64 {
 	atomic.AddUint64(manager.lastSessionID, 1)
 	return manager.lastSessionID
 }
---
+++ sessionmanager/sessionmanager_test.go
@@ -0,0 +1,126 @@
+package sessionmanager_test
+
+import (
+	"net/http"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	melody "gopkg.in/olahol/melody.v1"
+
+	. "github.com/hamilton-lima/robolucha-services/sessionmanager"
+)
+
+var _ = Describe("Sessionmanager", func() {
+
+	Context("when creating the session manager", func() {
+		var manager *SessionManager
+
+		BeforeEach(func() {
+			manager = NewSessionManager()
+		})
+
+		It("should have empty list of sessions", func() {
+			var sessions = manager.GetSessions("")
+			Expect(len(sessions)).To(Equal(0))
+		})
+
+		It("should have empty list of sessions for different matchIDs", func() {
+			var sessions = manager.GetSessions("32")
+			Expect(len(sessions)).To(Equal(0))
+
+			sessions = manager.GetSessions("42")
+			Expect(len(sessions)).To(Equal(0))
+		})
+
+		var globalMatchID = "42"
+
+		var createSessionWithRequest = func() *melody.Session {
+			var session = &melody.Session{}
+			req, _ := http.NewRequest("GET", "/match/"+globalMatchID, nil)
+			session.Request = req
+			return session
+		}
+
+		It("should return the proper matchID from the URL", func() {
+			var session = createSessionWithRequest()
+			var matchID = manager.GetIDFromURL(session.Request.URL)
+			Expect(matchID).To(Equal(globalMatchID))
+		})
+
+		It("should have 1 element after adding to the list", func() {
+			manager.AddSession(createSessionWithRequest())
+			var sessions = manager.GetSessions(globalMatchID)
+			Expect(len(sessions)).To(Equal(1))
+		})
+
+		It("should have 2 element after adding to the list", func() {
+			manager.AddSession(createSessionWithRequest())
+			manager.AddSession(createSessionWithRequest())
+
+			var sessions = manager.GetSessions(globalMatchID)
+			Expect(len(sessions)).To(Equal(2))
+		})
+
+		It("should have 1 element after adding 2 and removing 1 from the list", func() {
+			var session1 = createSessionWithRequest()
+			var session2 = createSessionWithRequest()
+			manager.AddSession(session1)
+			manager.AddSession(session2)
+			manager.RemoveSession(session1)
+
+			var sessions = manager.GetSessions(globalMatchID)
+			Expect(len(sessions)).To(Equal(1))
+		})
+
+		It("should have 0 element after adding 2 and removing 2 from the list", func() {
+			var session1 = createSessionWithRequest()
+			var session2 = createSessionWithRequest()
+			manager.AddSession(session1)
+			manager.AddSession(session2)
+			manager.RemoveSession(session1)
+			manager.RemoveSession(session2)
+
+			var sessions = manager.GetSessions(globalMatchID)
+			Expect(len(sessions)).To(Equal(0))
+		})
+
+		It("nextID should generate values in sequence", func() {
+			Expect(*manager.NextID()).To(Equal(uint64(1)))
+			Expect(*manager.NextID()).To(Equal(uint64(2)))
+			Expect(*manager.NextID()).To(Equal(uint64(3)))
+		})
+
+		It("should have proper ID in the sessions after adding", func() {
+			var session1 = createSessionWithRequest()
+			var session2 = createSessionWithRequest()
+			var session3 = createSessionWithRequest()
+
+			Expect(manager.GetIDFromSession(session1)).To(Equal(0))
+			Expect(manager.GetIDFromSession(session2)).To(Equal(0))
+			Expect(manager.GetIDFromSession(session3)).To(Equal(0))
+
+			manager.AddSession(session1)
+			manager.AddSession(session2)
+			manager.AddSession(session3)
+
+			Expect(manager.GetIDFromSession(session1)).To(Equal(1))
+			Expect(manager.GetIDFromSession(session2)).To(Equal(2))
+			Expect(manager.GetIDFromSession(session3)).To(Equal(3))
+		})
+
+		It("should have valid elements after adding and removing from the list", func() {
+			var session1 = createSessionWithRequest()
+			var session2 = createSessionWithRequest()
+			manager.AddSession(session1)
+			manager.AddSession(session2)
+			manager.RemoveSession(session1)
+
+			var sessions = manager.GetSessions(globalMatchID)
+			var session = sessions[0]
+			Expect(session).ShouldNot(BeNil())
+
+		})
+
+	})
+
+})
