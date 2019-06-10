b10f76e583d81e24c334347d06d628a7b75a2fb9
Hamilton Lima <hamilton.lima@gmail.com>
2019-06-09 21:01:31 -0400

    Fixes go-get script

--- go-get.sh
+++ go-get.sh
@@ -1,5 +1,8 @@
 set -o xtrace
 
+go get -v gitlab.com/robolucha/robolucha-publisher/session
+go get -v gitlab.com/robolucha/robolucha-publisher/redis
+
 go get -v github.com/gomodule/redigo/redis
 go get -v github.com/gin-contrib/cors
 go get -v github.com/gin-gonic/gin
@@ -8,6 +11,3 @@ go get -v gopkg.in/olahol/melody.v1
 go get -v github.com/onsi/ginkgo
 go get -v github.com/hpcloud/tail
 go get -v github.com/onsi/gomega
-
-go get -v gitlab.com/robolucha/robolucha-publisher/session
-go get -v gitlab.com/robolucha/robolucha-publisher/redis
